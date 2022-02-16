cf7846c3987d6d07ef2ea377093d8282bb9d0ed7
Daniel Lopes <daniellopes0208@hotmail.com>
2022-02-16 13:54:44 -0300

    Icluindo projeto via terminal ubuntu

---
+++ src/br/com/dio/Linvros.java
@@ -0,0 +1,5 @@
+package br.com.dio;
+
+public class Linvros extends Livros {
+
+}
---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model1.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato (null, null, null);
+		Livros livros = new Linvros ();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		int a = 54;
+		int b = 10;
+		System.out.println("Hello Mundo " + (a+b));
+	}
+
+}
+
+
+class Livros {
+	private String nome;
+	private String npag;
+}
